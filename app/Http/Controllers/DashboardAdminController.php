e68ebbe24425fcc6ae8914f289c6375aa8dc186f
arjunamhv <mhv@students.unnes.ac.id>
2023-12-08 16:19:44 +0700

    fix(admin):fixing admin authorization adding admin auth

--- app/Http/Controllers/DashboardAdminController.php
+++ app/Http/Controllers/DashboardAdminController.php
@@ -4,19 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Models\Booking;
 use App\Models\Type;
+use App\Models\Gor;
+use App\Models\Field;
 use Carbon\Carbon;
 use App\Charts\VisitorStats;
 use Illuminate\Http\Request;
 
 class DashboardAdminController extends Controller
 {
-    public function index(VisitorStats $chart)
+    public function index(VisitorStats $chart, $userId)
     {
         $fiveDaysToGo = Carbon::now()->addDays(5)->endOfDay();
 
-        $bookings = Booking::where('status', ['Paid', 'CheckIn'])
-            ->whereDate('booking_date', '<=', $fiveDaysToGo)
-            ->get();
+        $gorData = Gor::where('user_id', $userId)->first();
+        $fieldsData = Field::where('gor_id', $gorData->id)->get();
+
+        if ($fieldsData) {
+            foreach ($fieldsData as $field) {
+                $bookingsData = Booking::where('field_id', $field->id);
+            }
+        } else {
+            $bookingsData = '';
+        }
+
+        $bookings = $bookingsData->where('status', ['Paid', 'CheckIn']);
+        foreach ($bookings as $booking) {
+            $booking->whereDate('booking_date', '<=', $fiveDaysToGo)->get();
+        }
+        
 
         foreach ($bookings as $booking) {
             $typeId = $booking->field->type;
@@ -24,8 +39,10 @@ class DashboardAdminController extends Controller
             $booking->field_type = $type->name;
         }
         $data['chart'] = $chart->build();
-        $totalTransaksi = Booking::whereNotIn('status', ['Unpaid', 'Cancled'])->count();
-        $getTotalAmount = Booking::whereNotIn('bookings.status', ['Unpaid', 'Canceled'])
+
+        $totalTransaksi = $bookingsData->whereNotIn('status', ['Unpaid', 'Cancled'])
+        ->count();
+        $getTotalAmount = $bookingsData
         ->join('payments', 'bookings.id', '=', 'payments.booking_id')
         ->sum('payments.amount');
         $totalAmount = 'Rp ' . number_format($getTotalAmount, 0, ',', '.');
--- app/Http/Controllers/MyGORController.php
+++ app/Http/Controllers/MyGORController.php
@@ -16,30 +16,6 @@ use Illuminate\Http\Request;
 class MyGORController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show($userId)
@@ -81,30 +57,5 @@ class MyGORController extends Controller
 
         }
         return view('admin.mygor', ['gorData' => $gorData, 'fieldsData' => $fieldsData]);
-    }
-
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(GOR $gor)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Gor $gor)
-    {
-        //
     }
 }
